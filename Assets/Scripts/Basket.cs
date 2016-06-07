8ef08616b00acc22abd5031453a252689acddcc4
christianjnegri <christianjnegri@gmail.com>
2016-06-07 12:38:08 -0400

    Keeps Score
    Game keeps score, _scene_1 added for difficulty.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -3,10 +3,7 @@ using System.Collections;
 
 public class Basket : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+	public GUIText scoreGT;
 	
 	// Update is called once per frame
 	void Update () {
@@ -18,10 +15,18 @@ public class Basket : MonoBehaviour {
 		pos.x = mousePos3D.x;
 		this.transform.position = pos;
 	}
+	void Start(){
+		GameObject scoreGO = GameObject.Find ("ScoreCounter");
+		scoreGT = scoreGO.GetComponent<GUIText> ();
+		scoreGT.text = "0";
+	}
 	void OnCollisionEnter(Collision coll) {
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") {
 			Destroy(collidedWith);
 		}
+		int score = int.Parse (scoreGT.text);
+		score += 100;
+		scoreGT.text = score.ToString ();
 	}
 }
