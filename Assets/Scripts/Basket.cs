b370ec3054c45ec7aba8f6e88f7239047bd65b4e
christianjnegri <christianjnegri@gmail.com>
2016-06-06 21:48:55 -0400

    Basket Movement
    The baskets now move, apples are destroyed if they fall too low.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -10,6 +10,12 @@ public class Basket : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//Get the current screen position of the mouse from Input
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		Vector3 pos = this.transform.position;
+		pos.x = mousePos3D.x;
+		this.transform.position = pos;
 	}
 }
