363c5e2fed14aa81a3fb25d8801716c9d31121c2
christianjnegri <christianjnegri@gmail.com>
2016-06-06 21:58:07 -0400

    Apples Caught
    Apples can be caught.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -18,4 +18,10 @@ public class Basket : MonoBehaviour {
 		pos.x = mousePos3D.x;
 		this.transform.position = pos;
 	}
+	void OnCollisionEnter(Collision coll) {
+		GameObject collidedWith = coll.gameObject;
+		if (collidedWith.tag == "Apple") {
+			Destroy(collidedWith);
+		}
+	}
 }
